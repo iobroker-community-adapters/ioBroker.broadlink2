60372962a704d71659d6174b225b6f23b7170e7f
soef <soef@gmx.net>
2017-03-20 17:00:27 +0100

    moved code to native.code

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ var utils = require(__dirname + '/lib/utils'),
 	currentDevice,
     inObjectChange;
 
+var reCODE = /^CODE_|^/;
+var reIsCODE = /^CODE_[a-f0-9]{16}/;
+var codes = {};
+var defaultName = '>>> Learned, please describe';
+
 adapter.on('unload', function (callback) {
 	try {
 		if (currentDevice) {
@@ -19,7 +24,11 @@ adapter.on('unload', function (callback) {
 		callback();
 	}
 }).on('objectChange', function (id, obj) {
-     if (!id || !obj) return;
+     if (!id || !obj) {
+         if (id) delete codes[id];
+         return;
+     }
+     addCode(id, obj);
      if (obj.type === 'channel' || obj.type === 'device' && !inObjectChange) {
          inObjectChange = true;
          createLerningModeState (id, function () {
@@ -51,7 +60,7 @@ adapter.on('unload', function (callback) {
             enterLeaningMode (aktChannel, name);
             break;
         case 'sendCode':
-            var code = state.val.replace(/^CODE_|^/, 'CODE_');
+            var code = state.val.replace(reCODE, 'CODE_');
             if (isSignalCode (code)) sendCode (code);
             break;
         default:
@@ -59,18 +68,22 @@ adapter.on('unload', function (callback) {
             
             if (isSignalCode (command)) {
                 sendCode (command);
-            } else {
-                // If the object is not a signal code, check the name of the object
-                adapter.getObject (id, function (err, obj) {
-                    if (err) {
-                        adapter.log.error (err);
-                    } else if (obj.common.name) {
-                        if (isSignalCode (obj.common.name)) {
-                            sendCode (obj.common.name)
-                        }
-                    }
-                });
+            } else if (codes[id]) {
+                sendCode (codes[id]);
             }
+            //     // If the object is not a signal code, check the name of the object
+            //     adapter.getObject (id, function (err, obj) {
+            //         if (err || !obj) {
+            //             adapter.log.error (err);
+            //         } else if (obj.native && obj.native.keyCode) {
+            //             sendCode (obj.native.keyCode);
+            //         } else if (obj.common.name) {
+            //             if (isSignalCode (obj.common.name)) {
+            //                 sendCode (obj.common.name)
+            //             }
+            //         }
+            //     });
+            // }
     }
     
 // }).on('stateChange', function (id, state) {
@@ -141,13 +154,17 @@ adapter.on('unload', function (callback) {
 });
 
 function sendCode(value) {
-	var buffer = new Buffer(value.substr(5), 'hex'); // substr(5) removes CODE_ from string
-	currentDevice.sendData(buffer);
+    var buffer = new Buffer(value.replace(reCODE, ''), 'hex');
+    //var buffer = new Buffer(value.substr(5), 'hex'); // substr(5) removes CODE_ from string
+	
+    currentDevice.sendData(buffer);
 	adapter.log.debug('sendData to ' + currentDevice.host.address + ', Code: ' + value);
 }
 
 function isSignalCode(value) {
-	return value.length > 21 && value.substr(0, 5) == 'CODE_';
+    return reIsCODE.test(value);
+	//return value.length > 21 && value.substr(0, 5) == 'CODE_';
+	
 }
 
 function enterLeaningMode(aktChannel, name) {
@@ -162,10 +179,11 @@ function enterLeaningMode(aktChannel, name) {
         if (leaveTimer) {
             //clearTimeout (leaveTimer);
             leaveTimer = null;
+            
         }
         clearInterval(timer);
+        currentDevice.emitter.removeAllListeners('rawData');
         adapter.setState((aktChannel ? aktChannel + '.' : '') + 'enableLearningMode', {val: false, ack: true});
-        adapter.log.info('Left learning mode - aktChannel=' + aktChannel);
     });
     
     currentDevice.enterLearning();
@@ -182,27 +200,32 @@ function enterLeaningMode(aktChannel, name) {
         leaveLearningMode();
         
         var id = aktChannel ? aktChannel : namespaceChannelLearned;
-        id += '.CODE_' + hex;
+        //id += '.CODE_' + hex; // see remarks at checkMigrateStates
+        id += '.' + name;
         // Before create new object check one with the same id already exists
         adapter.getObject(id, function (err, obj) {
             if (err) {
                 adapter.log.error(err);
             } else {
-                if (!obj) {
+                // see remarks at checkMigrateStates
+                //if (!obj) {
                     adapter.setObject(id, {
                         type: 'state',
                         common: {
-                            name: name ? name : '>>> Learned, please describe',
+                            name: name ? name : defaultName,
                             type: 'boolean',
                             role: '',
                             read: false,
                             write: true
                         },
-                        native: {}
+                        native: {
+                            code: hex
+                        }
                     });
+                if (!obj) {
                     adapter.log.info('New IR-Code created in ' + namespaceChannelLearned);
                 } else {
-                    adapter.log.info('IR-Code already exists: ' + obj.common.name);
+                    adapter.log.info('IR-Code owerwritten: ' + obj.common.name);
                 }
             }
         });
@@ -229,9 +252,71 @@ function createLerningModeState (path, cb) {
     
 }
 
+function addCode(id, obj) {
+    if (obj.type !== 'state') return false;
+    var code = isSignalCode (obj.common.name) ? obj.common.name.replace(reCODE, '') : obj.native ? obj.native.code : undefined;
+    if (code) codes[id] = code;
+}
+
+
+function checkMigrateStates(objs, cb) {
+    return cb && cb(); // remove this line,
+                       // if you want to move the (ir)code from the 'id' or 'name' to the obj.native.code property
+                       // this function will convert all existing states with a name or id != the nativeName // '>>> Learned, please describe'
+                       //
+                       // the id shouldn't be used because in this case the (ir)code can change. the native object is for own, native values
+
+    var objsArr = [];
+    for (var id in objs) {
+        objsArr.push (id);
+    }
+    
+    var doIt = function() {
+        if (objsArr.length <= 0) return cb && cb ();
+        var id = objsArr.pop ();
+        var obj = objs[id];
+        if (obj.native && obj.native.code) return doIt ();
+        var idCode = id.replace(/.*(CODE_.*$)/, '$1');
+        obj.native = obj.native || {};
+        if (isSignalCode (obj.common.name)) {
+            obj.native.code = obj.common.name;
+            var ar = id.split ('.');
+            obj.common.name = ar[ar.length - 1];
+            adapter.setObject (id, obj);
+        } else if (isSignalCode (idCode)) {
+            obj.native.code = idCode.replace(reCODE, '');
+            if (obj.common.name === defaultName) {
+                adapter.setObject(id, obj, doIt);
+                return;
+            }
+            var newId = id.replace(/(CODE_.*$)/, obj.common.name);
+            adapter.setObject (newId, obj, function (err, obj) {
+                adapter.getState (id, function (err, state) {
+                    if (err || !state) return adapter.delObject (id, doIt);
+                    adapter.setState (newId, state, function (err, state) {
+                        adapter.delState (id, function () {
+                            adapter.delObject (id, doIt);
+                        });
+                    })
+                })
+            })
+        } else {
+            doIt();
+        }
+    };
+    doIt();
+}
+
 function main() {
 	//adapter.log.info('Config IP-Address: ' + adapter.config.ip);
-
+    
+    adapter.getAdapterObjects(function (objs) {
+        for (var id in objs) {
+            addCode(id, objs[id]);
+        }
+        checkMigrateStates(objs);
+    });
+    
 	adapter.setObject(namespaceChannelLearned, {
 		type: 'channel',
 		common: {
